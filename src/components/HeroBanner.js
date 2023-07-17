74aaf2967438ac6bef7f91235509ea9d0511791e
thuotracy <tracyjacobs775@gmail.com>
2023-07-17 14:36:26 +0300

    created 3 more js files

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
