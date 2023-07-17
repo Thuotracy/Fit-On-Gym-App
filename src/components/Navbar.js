33da815aac4a9be60ea03c9ccd738df57e06d18d
thuotracy <tracyjacobs775@gmail.com>
2023-07-17 14:15:22 +0300

    added the logo and styled it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 20px' }}/>
+      </Link>
+    </Stack>
   )
 }
 
