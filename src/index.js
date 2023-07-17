70020cdcb5cfe2fb7358a60d21f7be4c6903efaf
thuotracy <tracyjacobs775@gmail.com>
2023-07-17 14:06:47 +0300

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
