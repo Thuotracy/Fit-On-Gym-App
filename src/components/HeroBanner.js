9f2be3a1485e15268447318532066406503e3b61
thuotracy <tracyjacobs775@gmail.com>
2023-07-17 15:58:03 +0300

    styled the banner section

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,36 +1,25 @@
 import React from 'react';
-import { Box, Stack, Typography, Button } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
+import HeroBannerImage from '../assets/images/banner.png';
 
-import HeroBannerImage from "../assets/images/banner.png"
+const HeroBanner = () => (
+  <Box sx={{ mt: { xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#002366" fontWeight="600" fontSize="26px">JUST DO IT!</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+        Hustle for that muscle
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+        The pain you feel today will be the strength you feel tomorrow.
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#1560BD', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#002366" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Exercise
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" style={{ width: '600px', height: '820px'}}/>
+  </Box>
+);
 
-const HeroBanner = () => {
-  return (
-    <Box
-        sx={{
-            mt: {lg: '212px', xs: '70px'},
-            ml: {sm: '50px'}
-        }}
-        position="relative"
-        p="20px"
-    >
-
-        <Typography color="#002366" fontWeight="600" fontSize="26px">
-            JUST DO IT!
-        </Typography>
-
-        <Typography fontWeight="700px" sx={{fontSize: {lg: "42px", xs: "38px"}}}>
-            Hustle for that muscle
-        </Typography>
-
-        <Typography fontSize="20px" lineHeight="35px" mb={3}>
-            The pain you feel today, is the strength you feel tomorrow
-        </Typography>
-
-        <Button variant="contained" href="exercises">Search Exercises</Button>
-        <img src={HeroBannerImage} alt="banner" className='hero-banner-image'/>
-    </Box>
-  )
-}
-
-export default HeroBanner
+export default HeroBanner;
