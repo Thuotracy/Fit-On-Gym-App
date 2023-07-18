c0a99d275d9315939711cbce16bf1d7bd340af89
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 15:01:24 +0300

    added styling

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -4,9 +4,25 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <Stack>
+    <Stack
+        type="button"
+        alignItems="center"
+        justifyContent="center"
+        className="bodyPart-card"
+        sx={
+            bodyPart === item ? {
+                borderTop: '4px solid #1560BD',
+                backgroundColor: '#fff',
+                borderBottomLeftRadius: '20px',
+                width: '270px',
+                height: '280px',
+                cursor: 'pointer',
+                gap: '47px',
+            } : ''
+        }
+    >
         <img src={Icon} alt="dumbbell" styles={{width: '40px', height: '40px'}}/>
     </Stack>
   )
