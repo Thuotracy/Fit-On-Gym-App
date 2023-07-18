81621b9f7fc23271cf175884d5137727714fb495
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 09:46:35 +0300

    made changes

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -7,11 +7,18 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
   const [search, setSearch] = useState('')
 
+  const[exercises, setExercises] = useState([])
+
   const handleSearch = async () => {
     if(search) {
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
 
-      console.log(exercisesData)
+      const SearchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.bodyPart.toLowerCase().includes(search)
+      )
     }
   }
 
