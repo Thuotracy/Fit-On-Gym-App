481cb69ed3bc90b48292c4a41c1b2896d9da1aa3
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 16:18:06 +0300

    created a new file

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
+
   return (
-    <div>Exercises</div>
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">
+        Showing Results
+      </Typography>
+
+      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
