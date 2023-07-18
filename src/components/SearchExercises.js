b36c30ed6707d32d3f3ef1ae5e44f408a35968bd
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 09:38:47 +0300

    added the api

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState} from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 const SearchExercises = () => {
   const [search, setSearch] = useState('')
 
   const handleSearch = async () => {
     if(search) {
-      // const exercisesData = await fetchData();
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      console.log(exercisesData)
     }
   }
 
