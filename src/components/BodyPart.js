b2e045dc3a47c7cf9dfdf73e06ed97760b1af47b
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 15:56:45 +0300

    made changes

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -4,6 +4,7 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png';
 
+
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
@@ -19,9 +20,15 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
             height: '280px',
             cursor: 'pointer',
             gap: '47px',
-        }}        
+        }}  
+        onClick={() => {
+            setBodyPart(item);
+            window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+        }}   
     >
-        <img src={Icon} alt="dumbbell" styles={{width: '40px', height: '40px'}}/>
+        <img src={Icon} alt="dumbbell" style={{width: '40px', height: '40px'}}/>
+
+        <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
     </Stack>
   )
 }
