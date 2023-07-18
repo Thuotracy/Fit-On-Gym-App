eaca1be136d0465c030fe4a5166b2c8249c170ea
thuotracy <tracyjacobs775@gmail.com>
2023-07-18 08:58:31 +0300

    created and styled the search exercises bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs: '30px'}}} mb="50px" textAlign="center">
+        Great Exercises You <br/>Should Try
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px'},
+            width : { lg: '800px', xs: '350px'},
+            backgroundColor : '#fff',
+            borderRadiuS: '40px'
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder='Search Exercises'
+          type="text"
+        />
+      </Box>
+    </Stack>
   )
 }
 
