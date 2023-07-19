e20ab493707b4ed8db269d7ddc12ab5cdc62a7ab
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 10:08:08 +0300

    created the bodypart button

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -6,7 +6,13 @@ const ExerciseCard = ({ exercise }) => {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-    </Link>
+
+      <Stack direction="row">
+      <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {exercise.bodyPart}
+      </Button>
+      </Stack>
+    </Link> 
   )
 }
 
