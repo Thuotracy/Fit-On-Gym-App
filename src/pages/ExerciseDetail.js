d37c1bc549d3b06b76255d83299bf35f27d7f180
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 10:54:31 +0300

    started working on the exercise details page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 
 const ExerciseDetail = () => {
   return (
