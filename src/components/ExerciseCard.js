0664fdb1a1e3ca9691bd8f0d7eec914f84b884ba
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 10:14:45 +0300

    made changes

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -8,10 +8,10 @@ const ExerciseCard = ({ exercise }) => {
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
 
       <Stack direction="row">
-        <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        <Button sx={{ ml: '21px', color: '#fff', background: '#1560BD', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
           {exercise.bodyPart}
         </Button>
-        <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        <Button sx={{ ml: '21px', color: '#fff', background: '#3F3F4E', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
           {exercise.target}
         </Button>
       </Stack>
