2c391d816433386adfa02cf73cbe3b36980e9eff
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 12:25:57 +0300

    created the Loader.js file

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 
+
+import HorizontalScrollbar from './HorizontalScrollbar'
+
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0'}}}>
