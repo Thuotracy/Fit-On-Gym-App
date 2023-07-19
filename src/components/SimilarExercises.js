e8c07ddf2a0c43fc26805ea68a04293c26c0ae2d
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 12:22:55 +0300

    started on similar exercises section

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
 
-const SimilarExercises = () => {
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box sx={{ mt: { lg: '100px', xs: '0'}}}>
+
+    </Box>
   )
 }
 
