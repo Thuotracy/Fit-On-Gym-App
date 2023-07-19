7f4ff1344b9bb25036f5b3be9b33cabfdc9b35d8
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 13:09:12 +0300

    made changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/Logo.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 20px' }}/>
+
+      {/* <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} /> */}
     </Stack>
     <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
   </Box>
