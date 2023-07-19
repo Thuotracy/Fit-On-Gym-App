74516628411813cd2398173f32d3a4f081473be1
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 11:11:41 +0300

    made changes

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,12 +4,29 @@ import { Box } from '@mui/material';
 
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import SimilarExercises from '../components/SimilarExercises';
 
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+    fetchExercisesData();
+  }, [id]);
+
+
+
   return (
     <Box>
-      <Details />
+      <Detail exerciseDetail={exerciseDetail} />
       <SimilarExercises />
     </Box>
   )
