1f16b192e3ab377fc222eaa85232a31134ce81ee
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 10:02:32 +0300

    created the exercise cards

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <Link>
-    
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
     </Link>
   )
 }
