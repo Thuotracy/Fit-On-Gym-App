1243e03bdbf0c5db0e938ac977eb54019dec8b7e
thuotracy <tracyjacobs775@gmail.com>
2023-07-19 10:08:45 +0300

    created the targetmuscle button

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -8,9 +8,12 @@ const ExerciseCard = ({ exercise }) => {
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
 
       <Stack direction="row">
-      <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
-        {exercise.bodyPart}
-      </Button>
+        <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+          {exercise.bodyPart}
+        </Button>
+        <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+          {exercise.target}
+        </Button>
       </Stack>
     </Link> 
   )
