cf11b0ceb5b83bdee7c9a3955c79d1c8e5b0d01c
thuotracy <tracyjacobs775@gmail.com>
2023-07-20 10:39:04 +0300

    made changes

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,14 +7,14 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
     <Typography sx={{ fontSize: { lg: '40px', xs: '20px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
-      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
+      Similar <span style={{ color: '#1560BD', textTransform: 'capitalize' }}>Target Muscle</span> exercises
     </Typography>
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
     </Stack>
     
     <Typography sx={{ fontSize: { lg: '40px', xs: '20px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
-      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
+      Similar <span style={{ color: '#1560BD', textTransform: 'capitalize' }}>Equipment</span> exercises
     </Typography>
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
