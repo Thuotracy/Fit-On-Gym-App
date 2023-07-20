6c2b462fef8eda9ba7ce90cbe08f6c53fa9ca4f1
thuotracy <tracyjacobs775@gmail.com>
2023-07-20 10:40:26 +0300

    styled the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
 
       {/* <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} /> */}
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '26x', xs: '18px' } }} mt="41px" textAlign="center" pb="40px">All Rights Reserved</Typography>
   </Box>
 );
 
