dc98c66e50232d91cc145009bf07d4bf1278fa0a
thuotracy <tracyjacobs775@gmail.com>
2023-07-21 10:30:17 +0300

    made changes

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -29,7 +29,10 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         <img src={Icon} alt="dumbbell" style={{width: '40px', height: '40px'}}/>
 
         <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
+
     </Stack>
+
+    
   )
 }
 
